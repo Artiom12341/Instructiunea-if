3b43d703b071a47d6448c3f11c8d355dad770187
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-10-18 01:16:55 +0300

    Add files via upload

---
+++ informatica inst.if.12.py
@@ -0,0 +1,9 @@
+a= int (input("nr.="))
+if a>=25:
+    print("M.Alb")
+if ((a>25) and(a<=50)):
+    print("M.Rosu")
+if ((a>50) and (a<=75)):
+    print("M.albastru")
+if ((a>75) and (a<=100)):
+    print("M.Negru")
