b66e23a0d727cfd5b1863833b56ce4585bb8816d
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-10-18 01:21:18 +0300

    Add files via upload

---
+++ informatica inst.if.14.py
@@ -0,0 +1,12 @@
+a= int (input("nr.="))
+if a>=25:
+    print("A")
+if ((a>25) and(a<=50)):
+    print("B")
+if ((a>50) and (a<=75)):
+    print("C")
+if ((a>75) and (a<=100)):
+    print("D")
+if ((a>100) and (a<=125)):
+    print("E")
+
