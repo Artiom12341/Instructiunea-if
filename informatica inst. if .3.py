39f75f33b943a8185c59a9d0f7156675e4009f43
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-10-18 01:04:11 +0300

    Add files via upload

---
+++ informatica inst. if .3.py
@@ -0,0 +1,9 @@
+a=int(input("pers. nr.1="))
+b=int(input("pers. nr.2="))
+c=int(input("pers. nr.3="))
+if ((a>18) and (a<60)):
+    print (a)
+if ((b>18) and (b<60)):
+    print (b)
+if ((c>18) and (c<60)):
+    print (c)
