4cab269b9f0c0bdd435cbfc41f4b1abf2ff592a0
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2020-10-18 01:13:58 +0300

    Add files via upload

---
+++ informatica inst .if .10.py
@@ -0,0 +1,15 @@
+a=int(input("nr.1="))
+b=int(input("nr.2="))
+c=int(input("nr.3="))
+if a%2==0:
+    print(a ,"-par")
+else:
+        print(a,"-impar")
+if b%2==0:
+    print(b ,"-par")
+else:
+        print(b,"-impar")
+if c%2==0:
+    print(c ,"-par")
+else:
+        print(c,"-impar")
